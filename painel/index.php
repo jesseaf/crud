23279197cbfa9fcda4e77c01a12f77e36cbd19ab
Jesse Alves <jesseaf_20@hotmail.com>
2021-09-02 14:43:13 -0300

    Criação da tela de login

---
+++ painel/index.php
@@ -0,0 +1,79 @@
+<?php include 'header.php'; ?>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-sm-12 col-md-12">
+            <h3><center>Contador de Cadastros</center></h3>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-sm-6 col-md-2">
+            <div class="panel panel-primary">
+                <div class="panel-heading">                    
+                    <center>Pessoas</center>
+                </div>
+                <div class="panel-body">              
+                    <center><small>total</small></center>      
+                    <center id="countPessoa"></center>                    
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-2">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <center>Bancos</center>
+                </div>
+                <div class="panel-body">         
+                    <center><small>total</small></center>           
+                    <center id="countBancos"></center>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-2">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <center>Profissões</center>
+                </div>
+                <div class="panel-body">
+                    <center><small>total</small></center>
+                    <center id="countProfissoes"></center>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-2">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <center>Pessoas</center>
+                </div>
+                <div class="panel-body">
+                    <center id="countPessoa"></center>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-2">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <center>Pessoas</center>
+                </div>
+                <div class="panel-body">
+                    <center id="countPessoa"></center>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6 col-md-2">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    <center>Pessoas</center>
+                </div>
+                <div class="panel-body">
+                    <center id="countPessoa"></center>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php include 'footer.php'; ?>
